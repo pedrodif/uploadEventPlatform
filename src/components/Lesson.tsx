36abacef7300f300b81e99d48dc2606c65556acf
Pedro Ferreira <pedrodif@icloud.com>
2022-06-22 00:25:36 -0300

    feat: :ambulance:

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -10,7 +10,7 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
   const isLessonAvailable = isPast(props.availableAt);
-  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mn", {
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR,
   });
 
