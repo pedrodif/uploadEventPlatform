14e9badc24ed7a2f5410cc9187f5975203808ba4
Pedro Ferreira <pedrodif@icloud.com>
2022-06-23 21:29:53 -0300

    feat: :art:

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 // em uri colocar o endereço da API
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ocmhgd1rz601z79ktpfrv7/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
